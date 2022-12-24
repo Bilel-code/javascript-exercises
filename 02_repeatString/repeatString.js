95ee9da65b61e0880767e19479d89c45f4ac9608
SneackyBillyy <bilel15113@gmail.com>
2022-12-24 23:08:10 +0100

    passed the 2 exercice ,tired i go to bed see you tomorrow

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string1,num) {
+    let string ='';
+    if (num <0){
+        return 'ERROR'
+    }else{
+for(let i=0;i<num;i++){
+    string += string1;
+    
+}
+   return string;
+}
 };
 
 // Do not edit below this line
